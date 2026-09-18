64d033920733d96cb5ae699c63424cad3dcbd2ff
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-18 13:39:07 -0300

    remove Electron preload

--- app/preload/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('xcore', {
-  getConfig: () => ipcRenderer.invoke('xcore:get-config'),
-  saveConfig: (config) => ipcRenderer.invoke('xcore:save-config', config),
-  runDemo: (input) => ipcRenderer.invoke('xcore:run-demo', input)
-});
