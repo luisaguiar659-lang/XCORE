5381a5808f428b2945f4bfc7d70874772bece617
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-18 13:39:18 -0300

    remove obsolete Electron config

--- core/config.js
+++
@@ -1,5 +0,0 @@
-const fs=require('fs');const path=require('path');const {app}=require('electron');
-function configPath(){return path.join(app.getPath('userData'),'config.json')}
-function loadConfig(){try{return JSON.parse(fs.readFileSync(configPath(),'utf8'))}catch{return {mode:'demo'}}}
-function saveConfig(patch){const current=loadConfig();const next={...current,...patch};fs.mkdirSync(path.dirname(configPath()),{recursive:true});fs.writeFileSync(configPath(),JSON.stringify(next,null,2),{mode:0o600});return next}
-module.exports={loadConfig,saveConfig};
